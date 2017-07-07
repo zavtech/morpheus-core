0e2b3bc196163568ac1eec7bf69e4ede4f0edaa9
zavster <Whtred4u?>
2017-07-06 22:21:25 -0400

    Add support to deploy to Maven Central via OSSRH

--- src/main/java/com/zavtech/morpheus/stats/StatsCollector.java
+++ src/main/java/com/zavtech/morpheus/stats/StatsCollector.java
@@ -1,8 +1,30 @@
+/**
+ * Copyright (C) 2014-2017 Xavier Witdouck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zavtech.morpheus.stats;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A class that can compute multiple stats as values are added.
+ *
+ * <p><strong>This is open source software released under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache 2.0 License</a></strong></p>
+ *
+ * @author  Xavier Witdouck
+ */
 public class StatsCollector {
 
     private Map<StatType,Statistic1> statMap = new HashMap<>();
